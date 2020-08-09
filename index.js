79047481da09bdd6354e18e4425d32d1aafc5324
brennank14 <brennank14@gmail.com>
2020-08-09 16:24:15 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array){
+  let result = array.find(game => game.result === "W")
+    return !!result ? result.year : undefined
+
+}
