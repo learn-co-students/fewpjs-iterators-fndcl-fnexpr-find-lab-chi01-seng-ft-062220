fdad8cafcb4bf852c63d057fcb201d491dc42768
foxten <tenaysiafox@gmail.com>
2020-08-10 11:34:17 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record){ 
+  let win = record.find(function(record) {return record.result === "W"})
+    if (win){
+    return win.year
+    }
+  }
